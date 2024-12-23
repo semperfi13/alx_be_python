1824c7a6fec4b72c79643fe441351e6a4bb85e96
Adamou Nikiema <adamsnikiema187@gmail.com>
2024-12-23 04:59:28 -0800

    4. Personal Daily Reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -6,7 +6,27 @@ time_bound = input("Is it time-bound? (yes/no): ").lower()
 
 match priority:
 
-    case "high" | "medium" | "low":
+    case "high":
+        if time_bound == "yes":
+            print(
+                f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!"
+            )
+        else:
+            print(
+                f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time"
+            )
+            exit()
+    case "medium":
+        if time_bound == "yes":
+            print(
+                f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!"
+            )
+        else:
+            print(
+                f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time"
+            )
+            exit()
+    case "low":
         if time_bound == "yes":
             print(
                 f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!"
