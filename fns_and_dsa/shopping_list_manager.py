899b4233aaefe02cce98e93c2aa10e033297c53d
adamou <adamsnikiema187@gmail.com>
2025-01-09 10:33:35 +0000

    1. Shopping List Manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -14,7 +14,7 @@ def main():
 
         if choice == "1":
             # Prompt for and add an item
-            item = input("Add your item: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             pass
         elif choice == "2":
