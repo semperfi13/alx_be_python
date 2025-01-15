cc57efb6e3598eefae43258f43a243acba782c9e
adamou <adamsnikiema187@gmail.com>
2025-01-15 17:44:46 +0000

    0. Create a Simple Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -6,10 +6,10 @@ class BankAccount:
         self.account_balance += amout
 
     def withdraw(self, amount):
-        if self.account_balance >= amount:
+        if self.account_balance > amount:
             return True
         else:
             return False
 
     def display_balance(self):
-        print(f"Your current account balance is {self.account_balance}")
+        print(f"Current Balance: ${self.account_balance}")
