cdd5f00cce2280f76911f69017dc88e67b8ccf14
adamou <adamsnikiema187@gmail.com>
2025-01-18 14:01:50 +0000

    3. Implementing Basic OOP for a Library Management System

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -10,10 +10,9 @@ class Book:
 
 
 class Library:
-    _books = []
 
     def __init__(self):
-        pass
+        self._books = []
 
     def add_book(self, book):
         self._books.append(book)
