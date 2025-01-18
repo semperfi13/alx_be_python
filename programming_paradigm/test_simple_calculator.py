8bb3328ce4961dcfc5544eb6fd345795661d3587
adamou <adamsnikiema187@gmail.com>
2025-01-18 11:20:42 +0000

    2. Writing Unit Tests for a Simple Calculator Class

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,32 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+
+class TestClass(unittest.TestCase):
+
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        result = self.calc.add(3, 6)
+        self.assertEqual(result, 9)
+
+    def test_subtract(self):
+        result = self.calc.subtract(3, 6)
+        self.assertEqual(result, -3)
+
+    def test_multiply(self):
+        result = self.calc.multiply(3, 6)
+        self.assertEqual(result, 18)
+
+    def test_divide(self):
+        result = self.calc.divide(10, 2)
+        self.assertEqual(result, 5)
+
+    def test_divide_by_zero(self):
+        self.assertIsNone(self.calc.divide(10, 0), None)
+
+
+if __name__ == "__main__":
+    unittest.main()
