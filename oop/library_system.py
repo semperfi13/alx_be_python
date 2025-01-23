4fc2d06352756c4b0b7454551a85fb8744e3034b
adamou <adamsnikiema187@gmail.com>
2025-01-23 08:45:40 +0000

    1. Mastering Inheritance and Composition in Python

--- oop/library_system.py
+++ oop/library_system.py
@@ -14,7 +14,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f"EBook: {self.title} by {self.author}, File size: {self.file_size}KB"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
 class PrintBook(Book):
@@ -25,7 +25,7 @@ class PrintBook(Book):
 
     def __str__(self):
         return (
-            f"PrintBook: {self.title} by {self.author}, Page count: {self.page_count}"
+            f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
         )
 
 
