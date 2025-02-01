ea18dbc51caa7604042afc3c937c4161e5a9a9ae
adamou <adamsnikiema187@gmail.com>
2025-02-01 16:20:10 +0000

    0. A Magical Database for Your Dream Online Bookstore Adventure!

---
+++ Intro_to_DB/alx_book_store.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+
+CREATE TABLE IF NOT EXISTS Books(
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id),
+    price DOUBLE,
+    publication_date DATE
+
+);
+
+CREATE TABLE IF NOT EXISTS Authors(
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS Customers(
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT  NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Orders(
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    order_date DATE
+);
+
+CREATE TABLE IF NOT EXISTS Order_Details(
+    orderdetailid INT PRIMARY KEY,
+    order_id INT,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    book_id INT,
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
+    quantity DOUBLE
+)
